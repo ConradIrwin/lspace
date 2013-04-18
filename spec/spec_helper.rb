873e7b6d3eb45ae479f3325f4c3eb1d54d12950a
Conrad Irwin <conrad.irwin@gmail.com>
2013-04-17 21:22:23 -0700

    use builtin rspec isolation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,5 @@
 require_relative '../lib/lspace'
 require_relative '../lib/lspace/eventmachine'
 require_relative '../lib/lspace/celluloid'
+require_relative '../lib/lspace/rspec'
 require 'pry-rescue/rspec'
-
-RSpec.configure do |c|
-  c.around(:each) do |example|
-    LSpace.clean do
-      example.run
-    end
-  end
-end
